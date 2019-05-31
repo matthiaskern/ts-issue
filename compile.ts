5999b2568efeb736a61c9925bde45cf5f1414b3f
Matthias Kern <matthias.kern@reaktor.com>
2019-05-31 16:46:14 +0300

    Share program for compilation

--- compile.ts
+++ compile.ts
@@ -1,17 +1,78 @@
 import * as ts from "typescript";
 
-let program = ts.createProgram(
-  ["packages/foo/index.ts", "packages/bar/index.ts", "packages/bar/baz.ts"],
-  {
-    noEmitOnError: true,
-    include: "packages/foo"
+const compilerOptions = {
+  noEmitOnError: true,
+  include: "packages/foo"
+};
+
+const filesA = [
+  "packages/foo/index.ts",
+  "packages/bar/index.ts",
+  "packages/bar/baz.ts"
+];
+
+const filesB = [
+  "packages/bar/index.ts",
+  "packages/bar/baz.ts",
+  "packages/foo/index.ts"
+];
+
+const prepareCompilation = (program: ts.Program) => (fileNames: string[]) => {
+  for (const filePath of fileNames) {
+    const sourceFile = program.getSourceFile(filePath);
+    if (sourceFile !== undefined) {
+      const emitResult = program.emit(sourceFile);
+      console.log({ emitResult });
+
+      console.log("\n\n");
+      console.log(
+        `Compilation ${
+          emitResult.emitSkipped ? "failed" : "worked"
+        } for ${filePath}!`
+      );
+
+      if (emitResult.emitSkipped) {
+        console.log("\n\n");
+        let sourceFiles = {
+          foo: program.getSourceFile("packages/foo/index.ts"),
+          bar: program.getSourceFile("packages/bar/index.ts"),
+          baz: program.getSourceFile("packages/bar/baz.ts")
+        };
+        console.log("\n\n");
+        console.log({ sourceFiles, program });
+        console.log("\n\n");
+        throw new Error("Found issue");
+      }
+    }
   }
-);
 
-let emitResult = program.emit();
+  let sourceFiles = {
+    foo: program.getSourceFile("packages/foo/index.ts"),
+    bar: program.getSourceFile("packages/bar/index.ts"),
+    baz: program.getSourceFile("packages/bar/baz.ts")
+  };
 
-let allDiagnostics = ts
-  .getPreEmitDiagnostics(program)
-  .concat(emitResult.diagnostics);
+  console.log("\n\n");
+  console.log({ sourceFiles, program });
+  console.log("\n\n");
+};
 
-console.log({ emitResult, allDiagnostics, program });
+// variant 1
+let program = ts.createProgram(["/packages/bar"], compilerOptions);
+
+let compile = prepareCompilation(program);
+
+compile(filesA);
+compile(filesB);
+
+console.log("variant 1 worked");
+
+// variant 2
+program = ts.createProgram(["./packages/foo"], compilerOptions);
+
+compile = prepareCompilation(program);
+
+compile(filesA);
+compile(filesB);
+
+console.log("variant 2 worked");
